6af9ea8f2b3e5d612f1747560317ba3c9d10e117
lsy3709 <lsy3709@naver.com>
2023-01-19 08:55:28 +0900

    시큐리티 설정 관련 주석 자료 수집 중 1

--- AndroidProject_team4_plantalk_spring_server/src/main/java/com/and4/travel_server/config/CorsConfig.java
+++ AndroidProject_team4_plantalk_spring_server/src/main/java/com/and4/travel_server/config/CorsConfig.java
@@ -9,6 +9,15 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+//	Cross Origin에서 자원을 요청하기 위해서는 다음과 같은 과정을 거친다.
+//
+//	HTTP 통신 헤더인 Origin 헤더에 요청을 보내는 곳의 정보를 담고 서버로 요청을 보낸다.
+//	이후 서버는 Access-Control-Allow-Origin헤더에 허용된 Origin이라는 정보를 담아 보낸다
+//	클라이언트는 헤더의 값과 비교해 정상 응답임을 확인하고 지정된 요청을 보낸다.
+//	서버는 요청을 수행하고 200OK 코드를 응답한다.
+	
+	//Class CorsFilter 공식 문서
+	//https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/filter/CorsFilter.html
 	@Bean
 	public CorsFilter corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
--- AndroidProject_team4_plantalk_spring_server/src/main/java/com/and4/travel_server/config/SecurityConfig.java
+++ AndroidProject_team4_plantalk_spring_server/src/main/java/com/and4/travel_server/config/SecurityConfig.java
@@ -13,7 +13,10 @@ import com.and4.travel_server.config.jwt.JwtAuthenticationFilter;
 import com.and4.travel_server.config.jwt.JwtAuthorizationFilter;
 import com.and4.travel_server.service.UserService;
 
+//해당 클래스를 Configuration으로 등록한다.
 @Configuration
+
+//스프링시큐리티 사용을 위한 어노테이션선언
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -23,31 +26,47 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private CorsConfig corsConfig;
 	
+	
+	
+//- BCryptPasswordEncoder는 BCrypt 해싱 함수(BCrypt hashing function)를 
+	//사용해서 비밀번호를 인코딩해주는 메서드와 
+	//사용자의 의해 제출된 비밀번호와 저장소에 저장되어 있는 비밀번호의 일치 여부를 확인해주는 메서드를 제공합니다.
+
+//- PasswordEncoder 인터페이스를 구현한 클래스입니다.
+//
+//- 생성자의 인자 값(verstion, strength, SecureRandom instance)을 통해서 해시의 강도를 조절할 수 있습니다.
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		
 		return new BCryptPasswordEncoder();
 	}
 	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		//hasRole() or hasAnyRole()
+		//특정 권한을 가지는 사용자만 접근할 수 있습니다
+		
+		//Role은 역할이고 Authority는 권한이지만 사실은 표현의 차이입니다. 
+		//Role은 “ADMIN”으로 표현하고 Authority는 “ROLE_ADMIN”으로 표기합니다.
 		
 		http
 		.addFilter(corsConfig.corsFilter())
-		.csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.csrf().disable() //csrf 보안 설정을 비활성화 , 해당 기능을 사용하기 위해서는 프론트단에서 csrf토큰값 보내줘야함
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // SessionCreationPolicy.STATELESS     - 스프링시큐리티가 생성하지도않고 기존것을 사용하지도 않음        ->JWT 같은토큰방식을 쓸때 사용하는 설정 
 		.and()
-		.httpBasic().disable()
+		.httpBasic().disable() //사용자 인증방법으로는 HTTP Basic Authentication을 사용 안한다.
 		.addFilter(new JwtAuthenticationFilter(authenticationManager()))
 		.addFilter(new JwtAuthorizationFilter(authenticationManager(), userService))
 		.authorizeRequests()
-		.antMatchers("/api/user/**")
-			.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+		.antMatchers("/api/user/**") 	// 스프링 시큐리티 룰을 무시하게 하는 Url 규칙(여기 등록하면 규칙 적용하지 않음)
+			.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')") //ROLE_USER, ROLE_MANAGER, ROLE_ADMIN이라는 롤을 가진 사용자만 접근 허용
 		.antMatchers("/api/manager/**")
 			.access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
 		.antMatchers("/api/admin/**")
 			.access("hasRole('ROLE_ADMIN')")
-		.anyRequest().permitAll();
+		.anyRequest().permitAll(); // 위에 권한 접근 외에 어떤 요청도 다 허용함. 
+		
 		
 	}
 }
